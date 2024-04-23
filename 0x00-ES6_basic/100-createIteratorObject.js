0781d5495ac6a2d6f74cc2ff3a9a1273ea95da99
dideey <bradleyjd32@gmail.com>
2024-04-23 03:54:16 +0300

    objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  const names = [];
+  for (const department of Object.keys(report.allEmployees)) {
+    for (const emp of report.allEmployees[department]) {
+      names.push(emp);
+    }
+  }
+
+  return names;
+}
