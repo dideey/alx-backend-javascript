b3dc2dca7d4da3f7ab2448882816bd544637be93
dideey <bradleyjd32@gmail.com>
2024-04-26 00:59:29 +0300

    getter

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building {
+  constructor(sqft = 0) {
+    if (
+      this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get getSqft() {
+    return this._sqft;
+  }
+}
