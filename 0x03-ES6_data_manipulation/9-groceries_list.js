96561b7fe02c3dafa3cee44edc8343a856ba579f
dideey <bradleyjd32@gmail.com>
2024-04-30 19:21:42 +0300

    map object

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+export default function groceriesList () {
+  const newmap = new Map([
+    ['Apples', '10'],
+    ['Tomatoes', '10'],
+    ['Pasta', '1'],
+    ['Rice', '1'],
+    ['Banana', '5'],
+  ]);
+  return newmap;
+}
