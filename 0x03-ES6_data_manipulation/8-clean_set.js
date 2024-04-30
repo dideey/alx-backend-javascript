a54d2cdd8b094a787878deb64f11b8cfed331962
dideey <bradleyjd32@gmail.com>
2024-04-30 22:32:27 +0300

    concatnate

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
-    return '';
+  const word = '';
+  if (startString && typeof startString === 'string') {
+    return Array.from(set)
+      .filter((value) => value.startsWith(startString))
+      .map((value) => value.slice(startString.length))
+      .join('-');
   }
-  const filteredValues = Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
-
-  return filteredValues.join('-');
+  return word;
 }
