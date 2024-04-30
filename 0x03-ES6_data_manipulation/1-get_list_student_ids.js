c3558588732dbf199805a2a07170b49a4ccf9e33
dideey <bradleyjd32@gmail.com>
2024-04-30 14:20:54 +0300

    map method

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+export default function getListStudentIds (student) {
+  if (!Array.isArray(student)) {
+    return [];
+  }
+  return student.map((student) => student.id);
+}
